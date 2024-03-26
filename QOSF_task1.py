cbf21046ba8af211e4a08483afaf64a25990a3cf
Prisha Singh <prishsingh@berkeley.edu>
2024-03-25 23:02:29 -0500

    Adding task file

---
+++ QOSF_task1.py
@@ -0,0 +1,8 @@
+#Given a positive integer “k” and a list of integer numbers, look for the numbers within the list, that are less than k. 
+#Consider an appropriate number of qubits and explain why your proposal is valid for all kinds of numbers in case 
+
+import numpy as np
+from qiskit import QuantumCircuit
+def less_than_k(k, lst_n):
+    
+    return 
